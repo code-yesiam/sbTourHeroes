fa46dca99b7895994d653e95d59349e272543225
yes i am <eugustagoncalves@gmail.com>
2023-09-12 18:57:27 -0300

    Final Angular Tutorial Tour of Heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,6 +2,11 @@ import { Component, Input } from '@angular/core';
 
 import { Hero } from "../hero";
 
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,5 +15,33 @@ import { Hero } from "../hero";
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.getHero()
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero=hero)
+  }
+
+  goBack(): void {
+    this.location.back()
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack())
+    }
+  }
 
 }
